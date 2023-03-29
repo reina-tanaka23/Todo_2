64691c1c6ffcad67e82d7a8ea119257af8b867b4
reina.tanaka <reina.tanaka0223@gmail.com>
2023-03-29 22:10:25 +0900

    Initial Commit

---
+++ Todo_2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Todo_2
+//
+//  Created by reina.tanaka on 2023/03/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo_2/Todo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_2App.swift
+//  Todo_2
+//
+//  Created by reina.tanaka on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct Todo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
